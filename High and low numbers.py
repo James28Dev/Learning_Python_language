adb044763439de312764c08566ee6044ab280a30
James28Dev <james28.dev@gmail.com>
2025-01-09 19:39:00 +0700

    update High and low numbers

---
+++ High and low numbers.py
@@ -0,0 +1,6 @@
+print("โปรแกรมหาเลขสูงสุด-ต่ำสุด")
+mylist = [10, 5, 8, 2, 15]
+min_value = min(mylist)
+max_value = max(mylist)
+print("ค่าต่ำสุดและสูงสุดใน list =", mylist)
+print("ค่าต่ำสุด:", min_value, "\tค่าสูงสุด:", max_value)
