33424996050ec0c7dfd4ad6a7e81c4d6a86acebe
James28Dev <james28.dev@gmail.com>
2025-01-09 19:47:56 +0700

    update Count words

--- Count words.py
+++ Count words.py
@@ -2,6 +2,6 @@ print("โปรแกรมนับคำ")
 txt = "this is a book"
 space = 1
 for char in txt:
-  if char == ' ':
-    space += 1
+    if char == ' ':
+        space += 1
 print(f"จากประโยค {txt} นับได้ {space} คำ")
