27cadf567266e6252372ad40dcf6d996e9272c27
James28Dev <james28.dev@gmail.com>
2025-01-09 20:16:13 +0700

    Update Sum of numbers that can be divided by 3, from 1 - n..py

--- Sum of numbers that can be divided by 3, from 1 - n..py
+++ Sum of numbers that can be divided by 3, from 1 - n..py
@@ -1,7 +1,9 @@
 print("โปรแกรมหาค่าผลรวมของเลขที่หาร 3 ลงตัว ตั้งแต่ 1 ถึงเลขที่ต้องการ")
-i = int(input("เลขสูงสุด : "))
-total = 0
-for num in range(1, i + 1):
-  if num % 3 == 0:
-    total += num
-print(f"ผลรวมของเลขที่หาร 3 ลงตัว ตั้งแต่ 1 - {i} คือ {total}")
+end_number = int(input("เลขสูงสุด : "))
+def sums_can_divided_by_3(end_num):
+  total = 0
+  for num in range(1, end_num + 1):
+    if num % 3 == 0:
+      total += num
+  return total
+print(f"ผลรวมของเลขที่หาร 3 ลงตัว ตั้งแต่ 1 - {end_number} คือ {sums_can_divided_by_3(end_number)}")
